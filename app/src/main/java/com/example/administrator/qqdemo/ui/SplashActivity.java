e245aaa8138a3f8fcb5488ec78cf904751848a89
冯国富 <fgz2016@outlook.com>
2017-05-10 01:47:07 +0800

    第二次提交，Splash界面布局实现

---
+++ app/src/main/java/com/example/administrator/qqdemo/ui/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.qqdemo.ui;
+
+import com.example.administrator.qqdemo.R;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ * 欢迎界面的activity
+ */
+
+public class SplashActivity extends BaseActivity{
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_splash;
+    }
+}
