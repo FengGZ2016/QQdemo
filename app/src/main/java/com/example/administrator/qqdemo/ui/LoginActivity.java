c06f1664f8acd645b3266aa69748c2712dc01751
冯国富 <fgz2016@outlook.com>
2017-05-10 02:20:36 +0800

    第三次提交，根据登录状态实现跳转

---
+++ app/src/main/java/com/example/administrator/qqdemo/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.qqdemo.ui;
+
+import com.example.administrator.qqdemo.R;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ * 登录界面
+ */
+public class LoginActivity extends BaseActivity{
+
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_login;
+    }
+}
--- app/src/main/java/com/example/administrator/qqdemo/ui/SplashActivity.java
+++ app/src/main/java/com/example/administrator/qqdemo/ui/SplashActivity.java
@@ -1,5 +1,9 @@
 package com.example.administrator.qqdemo.ui;
 
+import android.content.Intent;
+import android.os.Handler;
+
+import com.example.administrator.qqdemo.MainActivity;
 import com.example.administrator.qqdemo.R;
 
 /**
@@ -8,9 +12,59 @@ import com.example.administrator.qqdemo.R;
  */
 
 public class SplashActivity extends BaseActivity{
+    private Handler mhandler=new Handler();
+    private static final int DELAY=3000;//3秒
 
     @Override
     public int getLayoutResId() {
         return R.layout.activity_splash;
+    }
+
+
+    @Override
+    protected void init() {
+        super.init();
+        //检查登陆状态
+        if(checkLoginStatus()){
+            //已经登录，跳转到主界面
+            navigateToMain();
+
+        }else {
+            //没有登录，延时3秒，跳转到登录界面
+            navigateToLogin();
+        }
+    }
+
+
+    /**
+     * 跳转到登录界面
+     * */
+    private void navigateToLogin() {
+        mhandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAY);
+    }
+
+    /**
+     * 跳转到主界面
+     * */
+    private void navigateToMain() {
+        Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    /**
+     * 检查登录状态
+     * @return  true:已经登录，跳转到主界面   false:没有登录，跳转到登录界面
+     * */
+    private boolean checkLoginStatus() {
+        return false;
     }
 }
