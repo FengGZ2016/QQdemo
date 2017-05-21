6c5753f69cd3f7e36d38227a2a0a5e2245cdbfaa
冯国富 <fgz2016@outlook.com>
2017-05-21 13:11:44 +0800

    第五次提交，完成注册页面

--- app/src/main/java/com/example/administrator/qqdemo/ui/LoginActivity.java
+++ app/src/main/java/com/example/administrator/qqdemo/ui/LoginActivity.java
@@ -1,16 +1,56 @@
 package com.example.administrator.qqdemo.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.example.administrator.qqdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2017/5/10.
  * 登录界面
  */
-public class LoginActivity extends BaseActivity{
+public class LoginActivity extends BaseActivity {
 
+
+    @BindView(R.id.user_name)
+    EditText mUserName;
+    @BindView(R.id.password)
+    EditText mPassword;
+    @BindView(R.id.btn_login)
+    Button mBtnLogin;
+    @BindView(R.id.new_user)
+    TextView mNewUser;
 
     @Override
     public int getLayoutResId() {
         return R.layout.activity_login;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.new_user})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                break;
+            case R.id.new_user:
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/qqdemo/ui/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.qqdemo.ui;
+
+import com.example.administrator.qqdemo.R;
+
+/**
+ * Created by Administrator on 2017/5/17.
+ */
+public class RegisterActivity extends BaseActivity{
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_register;
+    }
+}
