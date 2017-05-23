cfb0b5ae85503fc223d0288b48026feb1a1d95ee
冯国富 <fgz2016@outlook.com>
2017-05-23 21:02:38 +0800

    第十三次提交，集成BottomBar底部导航栏

--- app/src/main/java/com/example/administrator/qqdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/qqdemo/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.administrator.qqdemo;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.qqdemo.ui.BaseActivity;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.BottomBarTab;
+import com.roughike.bottombar.OnTabSelectListener;
+
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+    //创建BottomBar监听器
+    private OnTabSelectListener mOnTabSelectListener=new OnTabSelectListener() {
+        @Override
+        public void onTabSelected(@IdRes int tabId) {
+
+        }
+    };
+
+    @BindView(R.id.fragment_container)
+    FrameLayout mFragmentContainer;
+    @BindView(R.id.bottomBar)
+    BottomBar mBottomBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutResId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void init() {
+        super.init();
+        initBadge();
+        //给bottomBar设置监听器
+        mBottomBar.setOnTabSelectListener(mOnTabSelectListener);
+    }
+
+    /**
+     * 初始化未读消息
+     * */
+    private void initBadge() {
+        BottomBarTab nearby = mBottomBar.getTabWithId(R.id.tab_conversation);
+        //设置未读消息5条
+        nearby.setBadgeCount(5);
+    }
+
 }
